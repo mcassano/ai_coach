6044e986874b21c1fe5886803ba02ed6bf3556f1
Michael Cassano <mcassano@gmail.com>
2022-12-04 22:21:02 -0700

    add a very simple up-down x 10 test for annotator

---
+++ tests/test_annotator.py
@@ -0,0 +1,48 @@
+import os
+import unittest
+
+import cv2
+
+from annotator import Annotator
+from pose import PoseDetector
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def load_frame(path):
+    return cv2.imread(os.path.join(THIS_DIR, path),
+                      cv2.IMREAD_COLOR)
+
+
+class TestAnnotator(unittest.TestCase):
+
+    def setUp(self):
+        self.pushup_up_file_name = 'pushup_starting_position.jpg'
+        self.pushup_down_file_name = 'pushup_down_position.jpg'
+
+        # When we run the frame through the pose finder
+        self.detector = PoseDetector()
+
+    def test_up_down_up_down_good_results(self):
+        annotator = Annotator([])
+
+        # Go up-down ten times and check that the results are correct
+        for idx in range(0, 10):
+            result = annotator.annotate_frame(
+                load_frame(self.pushup_up_file_name))
+            self.check_results([idx, 'Down', True, 100.0, True], result)
+
+            result = annotator.annotate_frame(
+                load_frame(self.pushup_down_file_name))
+            self.check_results([idx+0.5, 'Up', True, 0.0, True], result)
+
+    def check_results(self, expected, actual):
+        self.assertEqual(expected[0], actual.count)
+        self.assertEqual(expected[1], actual.feedback)
+        self.assertEqual(expected[2], actual.form)
+        self.assertEqual(expected[3], actual.per)
+        self.assertEqual(expected[4], actual.success)
+
+
+if __name__ == '__main__':
+    unittest.main()
