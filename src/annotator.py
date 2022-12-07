2f375ae502c122423d372c024d1117cb2b8232f1
Michael Cassano <mcassano@gmail.com>
2022-12-07 09:40:47 -0700

    initial rip out

--- src/annotator.py
+++ src/annotator.py
@@ -3,8 +3,7 @@ import logging
 import numpy as np
 from src.annotation_result import AnnotationResult
 from src.pose import PoseDetector
-from src.pose_landmark import (LEFT_ELBOW, LEFT_HIP, LEFT_KNEE, LEFT_SHOULDER,
-                               LEFT_WRIST)
+from src.pose_landmark import landmarks
 
 logger = logging.getLogger(__name__)
 
@@ -25,8 +24,11 @@ class Annotator:
         lm_list = self.detector.find_position(frame, False)
         all_points_in_frame = lm_list and all(
             lm_list[idx].in_frame
-            for idx in (LEFT_SHOULDER, LEFT_ELBOW, LEFT_WRIST,
-                        LEFT_HIP, LEFT_KNEE))
+            for idx in (landmarks['LEFT_SHOULDER'],
+                        landmarks['LEFT_ELBOW'],
+                        landmarks['LEFT_WRIST'],
+                        landmarks['LEFT_HIP'],
+                        landmarks['LEFT_KNEE']))
         # logger.debug(f'in_frame {all_points_in_frame} lm_list {lm_list}')
         logger.debug(f'in_frame {all_points_in_frame}')
         count = 0
@@ -34,31 +36,37 @@ class Annotator:
         success = False
         angles = {}
         if lm_list:
-            elbow = self.detector.find_and_draw_angle(
-                frame, LEFT_SHOULDER, LEFT_ELBOW, LEFT_WRIST)
-            shoulder = self.detector.find_and_draw_angle(
-                frame, LEFT_ELBOW, LEFT_SHOULDER, LEFT_HIP)
-            hip = self.detector.find_and_draw_angle(
-                frame, LEFT_SHOULDER, LEFT_HIP, LEFT_KNEE)
-            angles = {'elbow': elbow, 'shoulder': shoulder, 'hip': hip}
+            for angle in self.movement['angles']:
+                # something like "LEFT_SHOULDER,LEFT_ELBOW,LEFT_WRIST"
+                recipe_array = angle['recipe'].split(',')
+
+                angles[angle['name']] = self.detector.find_and_draw_angle(
+                    frame,
+                    landmarks[recipe_array[0]],
+                    landmarks[recipe_array[1]],
+                    landmarks[recipe_array[2]])
 
             # Percentage of success of push-up
-            self.per = np.interp(elbow, (90, 160), (0, 100))
+            self.per = np.interp(angles['elbow'], (90, 160), (0, 100))
 
             # Bar to show Push-up progress
-            bar = np.interp(elbow, (90, 160), (380, 50))
+            bar = np.interp(angles['elbow'], (90, 160), (380, 50))
 
-            logger.debug(f'elbow {elbow} shoulder {shoulder} hip {hip}')
+            logger.debug(f'elbow {angles["elbow"]} '
+                         f'shoulder {angles["shoulder"]} '
+                         f'hip {angles["hip"]}')
             # Check to ensure right form before starting the program
             self.right_form = self.right_form or (all_points_in_frame and (
-                    elbow > 160 and shoulder > 40 and hip > 160))
+                    angles['elbow'] > 160
+                    and angles['shoulder'] > 40
+                    and angles['hip'] > 160))
 
             # Check for full range of motion for the push-up
             if not all_points_in_frame:
                 self.feedback = 'Get In Frame'
             elif self.right_form:
                 if self.per == 0:
-                    if elbow <= 90 and hip > 160:
+                    if angles['elbow'] <= 90 and angles['hip'] > 160:
                         self.feedback = 'Up'
                         if self.direction == 0:
                             count = 0.5
@@ -66,7 +74,9 @@ class Annotator:
                     else:
                         self.feedback = 'Fix Form'
                 if self.per == 100:
-                    if elbow > 160 and shoulder > 40 and hip > 160:
+                    if angles['elbow'] > 160\
+                            and angles['shoulder'] > 40\
+                            and angles['hip'] > 160:
                         self.feedback = 'Down'
                         if self.direction == 1:
                             count = 0.5
