292696d539f17ae794bcb3d0ee9ce00cdc3e8233
Michael Cassano <mcassano@gmail.com>
2022-12-11 11:03:36 -0700

    fix audio not playing and can't see setting mid-movement

--- src/advice_steps.py
+++ src/advice_steps.py
@@ -4,9 +4,9 @@ from src.advice import Advice
 
 
 class AdviceSteps(Enum):
-    GET_IN_FRAME = Advice('Get In Frame', './audio/get_in_frame.mp3')
-    FIX_FORM = Advice('Fix Form', './audio/fix_form.mp3')
-    GOOD = Advice('Good', './audio/good.mp3')
-    UP = Advice('Up', './audio/up.mp3')
-    DOWN = Advice('Down', './audio/down.mp3')
-    DONE = Advice('Done', './audio/done.mp3')
+    GET_IN_FRAME = Advice('Get In Frame', 'get_in_frame.mp3')
+    FIX_FORM = Advice('Fix Form', 'fix_form.mp3')
+    GOOD = Advice('Good', 'good.mp3')
+    UP = Advice('Up', 'up.mp3')
+    DOWN = Advice('Down', 'down.mp3')
+    DONE = Advice('Done', 'done.mp3')
--- src/audio.py
+++ src/audio.py
@@ -6,6 +6,8 @@ from gtts import gTTS
 from playsound import playsound
 from src.advice_steps import AdviceSteps
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
 logger = logging.getLogger(__name__)
 
 
@@ -14,15 +16,18 @@ class Audio:
         self._last_audio_time = 0
 
     def _play_audio_file(self, path: str):
-        playsound(path, block=False)
+        logger.debug(f'path: {path}')
+        playsound(os.path.join(THIS_DIR, '../audio', path), block=False)
         self._last_audio_time = time.time()
 
     def _play_advice_step(self, step: AdviceSteps):
         self._play_audio_file(step.value.audio_path)
 
     def _play_count(self, count):
-        path = f'./audio/count/{count}.mp3'
-        if not os.path.exists(path):
+        # make sure count isn't a decimal: 1.0 --> 1
+        count = int(count)
+        path = f'../audio/count/{count}.mp3'
+        if not os.path.exists(os.path.join(THIS_DIR, '../audio', path)):
             # generate count audio
             tts = gTTS(str(count), lang='en', slow=False)
             tts.save(path)
