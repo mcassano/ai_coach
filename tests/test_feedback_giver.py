582db65ec5eb693dbfa9a36d0ad6b52de981df9d
Dan Frankowski <dfrankow@gmail.com>
2023-03-29 09:58:45 -0500

    pylint changes

--- tests/test_feedback_giver.py
+++ tests/test_feedback_giver.py
@@ -4,7 +4,6 @@ from src.feedback_giver import FeedbackGiver
 
 
 class TestAudio(unittest.TestCase):
-    # pylint: disable-next=no-self-use
     def test_play_count(self):
         audio = FeedbackGiver()
         # pylint: disable-next=protected-access
